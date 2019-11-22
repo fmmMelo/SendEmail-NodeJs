08d985bbca4e70e9d39583bbbcd9d4936b17aeb8
fmmMelo <felipe.moura@estudante.ifms.edu.br>
2019-11-22 14:01:57 -0300

    adicionando arquivos

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ var express = require('express'),
     app.post('/send-email', function (req, res) {
 
 						var transporter = nodeMailer.createTransport({
-				 		 service: 'Gmail',
+				 		 service: 'gmail',
 			  				auth: {
 			  					//Aqui eu criei um e-mail no Gmail (ele faz o papel do meu servidor de e-mail)
-			    				user: 'empresahipotetica@gmail.com',
-			    				pass: 'U37xwq*9'
+			    				user: 'doaonlineif@gmail.com',
+			    				pass: 'mfxaehqdamrlhsku'
 			 				 }
 						});
 
 			     		 var mailOptions = {
 			          		from: req.body.from, // AQUI VAI O ENDEREÇO PARA RESPONDER (que é extraído do formulário)
-			          		to: 'igormatos55555@gmail.com', //AQUI VOCÊ COLOCO E-MAIL DO DESTINATÁRIOA DA MENSAGEM
+			          		to: 'fmm.felipemelomoura@gmail.com', //AQUI VOCÊ COLOCO E-MAIL DO DESTINATÁRIOA DA MENSAGEM
 			          		subject: req.body.subject, // AQUI VAI O TÍTULO DO E-MAIL (que é extraído do formulário)
 			          		text: req.body.body, // AQUI VAI O CONTEÚDO DA MENSAGEM (que é extraído do formulário)
 			          		html: '<b>'+'Titulo da mensagem: '+'</b>' + req.body.subject + '<br>' + '<b>'+'Remetente: '+'</b>' + req.body.from + '<br>' + '<b>'+'Mensagem: '+'</b>' + req.body.body // AQUI VAI O ARQUIVO HTML QUE SERÁ ENVIADO PARA O DESTINATÁRIO DA MENSAGEM.
@@ -38,7 +38,8 @@ var express = require('express'),
 			              return console.log(error);
 			          }
 			          console.log(info.envelope.from);
-			              res.redirect('/');
+						  res.redirect('/');
+						  console.log('success');
 			          });
       });
           app.listen(port, function(){
